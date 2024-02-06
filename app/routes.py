65bb0083a7dc4d399cc6bd8a159f073396539f41
John Loveland <john.loveland@apexon.com>
2024-02-06 09:54:43 -0600

    fixed circular import issue

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,23 @@
-from app import app, UserForm
+from app import app, db
+from app.forms import UserForm
+from app.models import User
+import sqlalchemy as sa
+from flask import render_template, request
 
-from flask import render_template
 
-
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
     form = UserForm()
+    if request.method == 'POST' and form.validate_on_submit():
+            username = form.username.data
+            password = form.password.data
+
+
+            user = User(username = username, password = password)
+
+            db.session.add(user)
+            db.session.commit()
+        
+
     return render_template('index.html', title='User Form', form=form)
